853ba41e421944e9f610430e1068b6b7f5913a46
HugoKt22-byte <hugo.rh2022@gmail.com>
2025-09-22 13:18:29 +0000

    loops finalizado treinamento

---
+++ Xadrez_Intermediário/treinamento loops finalizado.c
@@ -0,0 +1,62 @@
+/*
+#include <stdio.h> 
+
+int main(){
+//LOOPS ALINHADOS UTILIZANDO FOR... 
+    for (int i = 1; i <= 10; i++){
+        for (int j = 1; j <= 10; j++){
+            printf ("%d\t", i * j); // 1x1 = 1, 1x2= 2.... 1x10 = 10 ; 2x1 = 2, 2x2 =4... ; 3x1.....
+        }
+        printf("\n");
+    }
+
+}*/
+//PARTE DE CIMA ESTUDADA...
+
+//LOOPS ALINHADOS UTILIZANDO WHILE
+/*
+#include <stdio.h>
+
+int main(){
+
+int a = 1;
+while (a <= 10){
+int b = 1;
+while (b <= 10){
+printf ("%d\t", a * b);
+b++;
+}
+a++;
+    printf ("\n");
+}
+
+}
+*/
+//PARTE DE CIMA ESTUDADA
+/*
+#include <stdio.h>
+
+    int main (){
+
+    int a = 1;
+do {
+    int b = 1;
+do {
+b++;
+printf ("%d\t", a * b);
+} while (b <= 10);
+a++;
+printf ("\n");
+} while (a <= 10);
+*/
+//LOOPS FINALIZADO
+
+
+
+
+
+
+
+
+}
+
